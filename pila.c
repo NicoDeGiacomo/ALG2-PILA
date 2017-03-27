4381fa5cbb80340557d9e059ad65ba4ef61306ce
Nicolas De Giacomo <nicolas.de.giacomo@gmail.com>
2017-03-27 16:05:15 -0300

    Done tests TDD beginning

--- pila.c
+++ pila.c
@@ -13,4 +13,36 @@ struct pila {
  *                    PRIMITIVAS DE LA PILA
  * *****************************************************************/
 
+pila_t* pila_crear(void){
+	pila_t* pila = malloc(sizeof(pila_t));
+	if (!pila)
+		return NULL;
+	//TODO: Tamaño como constante
+	pila->cantidad = 0;
+	pila->capacidad = 5;
+	pila->datos = malloc(5 * sizeof(void*));
+	if (!pila->datos)
+		return NULL;
+	return pila;
+}
+
+void pila_destruir(pila_t *pila){
+	return;
+}
+
+bool pila_esta_vacia(const pila_t *pila){
+	return false;
+}
+
+bool pila_apilar(pila_t *pila, void* valor){
+	return false;
+}
+
+void* pila_ver_tope(const pila_t *pila){
+	return malloc(sizeof(pila_t));
+}
+
+void* pila_desapilar(pila_t *pila){
+	return malloc(sizeof(pila_t));
+}
 // ...
--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -11,4 +11,35 @@ void pruebas_pila_alumno() {
     pila_t* ejemplo = NULL;
 
     print_test("Puntero inicializado a NULL", ejemplo == NULL);
+
+    /* Variables a utilizar*/
+    pila_t* pila = pila_crear();
+    int valor1 = 5;
+    int valor2 = 9;
+
+    /* Pruebas con pila vacia */
+    //TODO: Cambiar 1 a true, 0 a false
+    print_test("pila_esta_vacia es true", pila_esta_vacia(pila) == 1);
+    print_test("pila_ver_tope es NULL", pila_ver_tope(pila) == NULL);
+    print_test("pila_desapilar es NULL", pila_desapilar(pila) == NULL);
+    print_test("pila_apilar es true", pila_apilar(pila, &valor1) == 1);
+    print_test("pila_apilar es true", pila_apilar(pila, &valor2) == 1);
+    print_test("pila_esta_vacia es false", pila_esta_vacia(pila) == 0);
+    print_test("pila_desapilar devuelve el tope", pila_desapilar(pila) == &valor2);
+    print_test("pila_desapilar devuelve el tope", pila_desapilar(pila) == &valor1);
+    pila_destruir(pila);
+
+    /* Prueba redimension */
+    pila = pila_crear();
+    for (int i = 0; i <= 6; ++i){
+        print_test("pila_apilar es true", pila_apilar(pila, &valor1) == 1);
+    }
+    for (int i = 0; i <= 6; ++i){
+        print_test("pila_desapilar devuelve el tope", pila_desapilar(pila) == &valor1);
+    }
+    print_test("pila_ver_tope es NULL", pila_ver_tope(pila) == NULL);
+    print_test("pila_desapilar es NULL", pila_desapilar(pila) == NULL);
+
+    pila_destruir(pila);
+
 }
