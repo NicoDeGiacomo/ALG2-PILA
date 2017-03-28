f77b42df044e3b9293f9a4e551ae1ae65c9578da
Nicolas De Giacomo <nicolas.de.giacomo@gmail.com>
2017-03-28 16:20:09 -0300

    First approach

--- pila.c
+++ pila.c
@@ -1,6 +1,11 @@
 #include "pila.h"
 #include <stdlib.h>
 
+#define SIZE 10 //Capacidad inicial de la pila
+//TODO: poner esto como porcentaje y pasar a size_t al multiplicar
+#define REALLOC_WHEN 4 //Porcentage al cual la capacidad de la pila se reduce
+#define REALLOC_TO 2 //Porcentaje de reduccion de la capacidad de la pila
+
 /* Definición del struct pila proporcionado por la cátedra.
  */
 struct pila {
@@ -13,29 +18,37 @@ struct pila {
  *                    PRIMITIVAS DE LA PILA
  * *****************************************************************/
 
+bool redimensionar(pila_t* pila, size_t size);
+
 pila_t* pila_crear(void){
 	pila_t* pila = malloc(sizeof(pila_t));
 	if (!pila)
 		return NULL;
-	//TODO: Tamaño como constante
 	pila->cantidad = 0;
-	pila->capacidad = 5;
-	pila->datos = malloc(5 * sizeof(void*));
+	pila->capacidad = SIZE;
+	pila->datos = malloc(SIZE * sizeof(void*));
 	if (!pila->datos)
 		return NULL;
 	return pila;
 }
 
 void pila_destruir(pila_t *pila){
+	free(pila->datos);
+	free(pila);
 	return;
 }
 
 bool pila_esta_vacia(const pila_t *pila){
-	return false;
+	return pila->cantidad == 0;
 }
 
-bool pila_apilar(pila_t *pila, void* valor){
-	return false;
+bool pila_apilar(pila_t* pila, void* valor){
+	//Si ya esta al maximo, redimensiono antes de apilar
+	if (pila->cantidad == pila->capacidad)
+		if (!redimensionar(pila, pila->capacidad+SIZE))
+			return false;
+	pila->datos[pila->cantidad-1] = valor;
+	return true;
 }
 
 void* pila_ver_tope(const pila_t *pila){
@@ -43,6 +56,17 @@ void* pila_ver_tope(const pila_t *pila){
 }
 
 void* pila_desapilar(pila_t *pila){
-	return malloc(sizeof(pila_t));
+	void* dato = pila->datos[pila->cantidad-1];
+	pila->cantidad--;
+	//Si baja del minimo, redimensiono luego de desapilar
+	if (pila->cantidad == pila->capacidad / REALLOC_WHEN)
+		redimensionar(pila, pila->capacidad / REALLOC_TO);
+	return dato;
 }
-// ...
+
+bool redimensionar(pila_t* pila, size_t size){
+	pila = realloc(pila, size);
+	if (!pila)
+		return false;
+	return true;
+}
--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -18,28 +18,25 @@ void pruebas_pila_alumno() {
     int valor2 = 9;
 
     /* Pruebas con pila vacia */
-    //TODO: Cambiar 1 a true, 0 a false
-    print_test("pila_esta_vacia es true", pila_esta_vacia(pila) == 1);
+    print_test("pila_esta_vacia es true", pila_esta_vacia(pila) == true);
     print_test("pila_ver_tope es NULL", pila_ver_tope(pila) == NULL);
     print_test("pila_desapilar es NULL", pila_desapilar(pila) == NULL);
-    print_test("pila_apilar es true", pila_apilar(pila, &valor1) == 1);
-    print_test("pila_apilar es true", pila_apilar(pila, &valor2) == 1);
-    print_test("pila_esta_vacia es false", pila_esta_vacia(pila) == 0);
+    print_test("pila_apilar es true", pila_apilar(pila, &valor1) == true);
+    print_test("pila_apilar es true", pila_apilar(pila, &valor2) == true);
+    print_test("pila_esta_vacia es false", pila_esta_vacia(pila) == false);
     print_test("pila_desapilar devuelve el tope", pila_desapilar(pila) == &valor2);
     print_test("pila_desapilar devuelve el tope", pila_desapilar(pila) == &valor1);
     pila_destruir(pila);
 
     /* Prueba redimension */
     pila = pila_crear();
-    for (int i = 0; i <= 6; ++i){
-        print_test("pila_apilar es true", pila_apilar(pila, &valor1) == 1);
-    }
-    for (int i = 0; i <= 6; ++i){
+    //TODO: ESTE SIZE EN CONSTANTE?
+    for (int i = 0; i <= 15; ++i)
+        print_test("pila_apilar es true", pila_apilar(pila, &valor1) == true);
+    for (int i = 0; i <= 15; ++i)
         print_test("pila_desapilar devuelve el tope", pila_desapilar(pila) == &valor1);
-    }
     print_test("pila_ver_tope es NULL", pila_ver_tope(pila) == NULL);
     print_test("pila_desapilar es NULL", pila_desapilar(pila) == NULL);
-
     pila_destruir(pila);
 
 }
